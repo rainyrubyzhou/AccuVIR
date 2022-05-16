bfa96bedd281e5f36bdb64ec227bcb3bbe2165a8
Runzhou <rainyrubyzhou@gmail.com>
2022-05-16 18:28:25 +0800

    updated code

---
+++ src/AccuVIR_main.py
@@ -0,0 +1,56 @@
+#################################################################################$$
+# Author: Runzhou
+# Driver Program for DBS and Branched Sampling. 
+# For iteratively constructing the graph, users need to mannually pass in the new backbone sequence.
+#################################################################################$$
+import sys
+import time
+import os
+import random
+import argparse
+import utils
+import beam_diverse as DBS
+import branch_sample as BS
+
+if __name__ == '__main__':
+    #parse args
+    BeamWidth = 500
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', type=str, required=True, help = "Reads file for graph construction(in fasta format).")
+    parser.add_argument('-b', type=str, required=True, help = "Backbone sequence file for graph construction (in fasta format).")
+    parser.add_argument('--beamwidth', type=int, default = 500,  required=False, help = "Beamwidth for diverse beam search (default: 500).")
+    
+    args = parser.parse_args()
+
+    print(args.r, args.b)
+    ec_reads = args.r
+    backbone = args.b
+    BeamWidth = args.beamwidth    
+    
+    graph_out_pref = (ec_reads + "_ON_" +\
+                     (os.path.splitext(os.path.basename(backbone))[0]) )
+    time1 = time.time()
+    aln_graph = utils.construct_aln_graph_from_fasta(ec_reads,  backbone, 5000)
+    time2 = time.time()
+    print("Finish building graph in %s s"%(time2 - time1))
+    # generate corresponding dagcon seq.
+    aln_graph.merge_nodes()
+    aln_graph.generate_consensus()
+    consen_str = aln_graph.consensus_str
+    consen_score = aln_graph.score(aln_graph.consensus_path)
+    with open(graph_out_pref + "_consen.fa", 'w') as consen_f:
+        consen_f.write(">consen_" + graph_out_pref + ': ' + str(consen_score) + '\n')
+        consen_f.write(consen_str + '\n')
+    nx_pack = utils.aln2nx(aln_graph)        
+    utils.nx2gfa(nx_pack[0], graph_out_pref + '.graph')
+    print("Running DBS of beadm width: ", BeamWidth)
+    DBS.Grouped_beam(nx_pack[0], BeamWidth, graph_out_pref + "_DBS_" + str(BeamWidth) + ".fa")
+    
+    #locate all homopolymer regions
+    utils.nx_homo(nx_pack[0], graph_out_pref + "_homo_loc.csv")
+    BS.sample(nx_pack[0], graph_out_pref, graph_out_pref + "_sampling.fa")
+    
+    # merge the output of two modules and filter out short ones for intermediate output(mainly from DBS).
+    utils.merge_filter(graph_out_pref + "_DBS_" + str(BeamWidth) + ".fa", graph_out_pref + "_sampling.fa", graph_out_pref + "_filtered.fa")
+    
+    
